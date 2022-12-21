427cc9aa5a04a38acdf09f3af6f8bea69ae4f629
SimonL26 <806985104@qq.com>
2022-12-21 14:13:09 +0000

    rewrite bot using discord.py ext commands

--- Scripts/bot.py
+++ Scripts/bot.py
@@ -1,5 +1,8 @@
 import discord
+from discord.ext import commands
 import responses
+import random 
+
 
 # async method to send message
 async def send_message(message, user_message, is_private:bool):
@@ -15,30 +18,58 @@ async def send_message(message, user_message, is_private:bool):
 def run_discord_bot():
     TOKEN = "MTA1NDQ4MTg5MzA0NDc4NTE2Mg.GzRjtC.-F72HmFY81wTT93dOS1VsJypotqxyHOF_jZALs"
 
+    description ="""A personal bot that is currently under development."""
+
     intents = discord.Intents.default()
+    intents.members = True
     intents.message_content = True
-    client = discord.Client(intents=intents)
 
-    @client.event
+    bot = commands.Bot(command_prefix=">", intents=intents, description=description)
+
+    @bot.event
     async def on_ready():
-        print(f'{client.user} is now running!')
+        print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+        print("----------")
 
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
-            return
-        
-        username = str(message.author)
-        user_message = str(message.content)
-        channel = str(message.channel)
+    @bot.command()
+    async def add(ctx, left: int, right: int):
+        """Adds two numbers together."""
+        await ctx.send(left+right)
 
-        print(f'{username} said: "{user_message} ({channel})"')
+    @bot.command()
+    async def roll(ctx, dice: str):
+        """Rolls a dice in NdN format."""
+        try:
+            rolls, limit = map(int, dice.split('d'))
+        except: 
+            await ctx.send("Format has to be in NdN!")
+            return 
 
-        if user_message[0] == '?':
-            user_message = user_message[1:]
-            await send_message(message, user_message, is_private=True)
-        else:
-            await send_message(message, user_message, is_private=False)
-    
-    
-    client.run(TOKEN)
+        result = ', '.join(str(random.randint(1, limit)) for _ in range(rolls))
+        await ctx.send(result)
+
+    @bot.command(description='For when you wanna settle the score some other way')
+    async def choose(ctx, *choices: str):
+        await ctx.send(random.choice(choices))
+
+    @bot.command()
+    async def repeat(ctx, times: int, content="repeating..."):
+        """Repeats a message multiple times"""
+        for i in range(times):
+            await ctx.send(content)
+
+    @bot.command()
+    async def joined(ctx, member: discord.Member):
+        await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+    @bot.group()
+    async def cool(ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+
+    @cool.command(name="bot")
+    async def _bot(ctx):
+        await ctx.send("Yes, the bot is cool")
+
+    bot.run(TOKEN)
+
--- Scripts/responses.py
+++ Scripts/responses.py
@@ -4,13 +4,13 @@ import random
 def handle_response(message:str) -> str:
     p_message = message.lower()
     
-    if p_message == "hello":
+    if p_message == "greet":
         return "Hello there!"
     
     if p_message == "roll":
         return str(random.randint(1, 6))
 
-    if p_message == "!help":
+    if p_message == "help":
         return "`This is some help message`"
 
     return "Confused..."
